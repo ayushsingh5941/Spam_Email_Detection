754560a31ae43ba65d2909226793381d04529020
Ayush <ayushsingh5941@gmai.com>
2020-01-02 17:12:30 +0530

    Initial commit

---
+++ SpamDetection.py
@@ -0,0 +1,7 @@
+# Spam Email detection with Naive Bayes (Binary classification problem)
+# Focus on classification problems
+# Classification is one of form of supervised learning
+
+# Bayes : It maps the probabilities of observing input features given belonging classes to the probability distribution
+# over classes based on bayes theorem
+# Naive : It simplifies the probability computation by assuming the predictive features are mutually independent
